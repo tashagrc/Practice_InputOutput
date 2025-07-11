228a149ce1ca533c05631af7ac2e8eedb5e65f01
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2025-07-11 09:00:50 +0700

    Initial Commit

---
+++ Practice_InputOutputTests/Practice_InputOutputTests.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_InputOutputTests.swift
+//  Practice_InputOutputTests
+//
+//  Created by Natasha Radika on 11/07/25.
+//
+
+import Testing
+@testable import Practice_InputOutput
+
+struct Practice_InputOutputTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
